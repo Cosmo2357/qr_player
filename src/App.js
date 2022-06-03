2ff818f57f40a14dd854d038e7a9c87c980612de
hayashi.yuhki <hayashi.yuhki@gmail.com>
2022-06-03 15:42:27 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import QRCode from "react-qr-code";
+import { useState, useEffect } from "react";
 
+//import Qr from './components/qr';
 function App() {
+  //make value of qr code and increment it every second and rerender the qr code
+  const [value, setValue] = useState("11111111");
+  const [perSec, setPerSec] = useState(1000);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const numValue = Number(value)
+      const newValue = numValue +1;
+      const newStringValue  = newValue.toString();
+      const newStringValuePadded = newStringValue.padStart(8, "0");
+      setValue(newStringValuePadded);
+    }, perSec);
+    return () => clearInterval(interval);
+  }, [value]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="App-header">
+        <div className="flex">
+        <button className="secButton" onClick={()=>{
+          if(perSec > 1000){
+          setPerSec(perSec-1000)
+          }
+          }}>&#x2212;</button>
+        <button className="secButton" onClick={()=>{setPerSec(perSec+1000)}}>&#x2b;</button>
+        </div>
+        <div style={{marginBottom: "24px"}}>{perSec / 1000 + "sec"}</div>
+        <div
+          style={{
+            height: "auto",
+            margin: "0 auto",
+            maxWidth: 180,
+            width: "100%",
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <QRCode
+            size={256}
+            style={{ height: "auto", maxWidth: "100%", width: "100%" }}
+            value={value}
+            viewBox={`0 0 256 256`}
+          />
+          <h3>ID: {value}</h3>
+        </div>
+      </div>
     </div>
   );
 }
